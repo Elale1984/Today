b6ee5ca6cec985212363b79e15c7a1cafec37b41
edwar <javaeddie84@gmail.com>
2022-06-17 00:03:07 -0500

    Set up View Binding Created instance of Firebase Auth

--- app/src/main/java/edu/gcu/today/SignIn.java
+++ app/src/main/java/edu/gcu/today/SignIn.java
@@ -3,12 +3,27 @@ package edu.gcu.today;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import edu.gcu.today.databinding.ActivitySignInBinding;
 
 public class SignIn extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
+
+    // Firebase
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = binding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
 }
