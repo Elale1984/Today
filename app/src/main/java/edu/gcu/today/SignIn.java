ca3d139cca8a096c8c7ca8177c27a998903bad9f
edwar <javaeddie84@gmail.com>
2022-06-17 00:21:42 -0500

    Working on Firebase sign in feature

--- app/src/main/java/edu/gcu/today/SignIn.java
+++ app/src/main/java/edu/gcu/today/SignIn.java
@@ -1,11 +1,20 @@
 package edu.gcu.today;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 import edu.gcu.today.databinding.ActivitySignInBinding;
 
@@ -25,5 +34,42 @@ public class SignIn extends AppCompatActivity {
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+
+        CheckForUserAuthenticationStatus();
+
+        binding.btnSignIn.setOnClickListener(view1 -> {
+            AuthenticateUser();
+        });
+    }
+
+    private void AuthenticateUser() {
+        String email = binding.oitEmail.toString();
+        String password = binding.oitPassword.toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                   if(task.isSuccessful()){
+                        NavigateToTodayMain();
+                   }
+                   else {
+                       IncorrectSignInDialog();
+                   }
+                });
+    }
+
+    private void IncorrectSignInDialog() {
+
+    }
+
+    private void CheckForUserAuthenticationStatus() {
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            NavigateToTodayMain();
+
+        }
+    }
+
+    private void NavigateToTodayMain() {
+        startActivity(new Intent(this, SignUp.class));
     }
 }
