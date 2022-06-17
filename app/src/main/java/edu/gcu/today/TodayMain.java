69c14c8e488cbc683d00952000a6ee79227588a7
edwar <javaeddie84@gmail.com>
2022-06-16 23:57:50 -0500

    Created sign in layout

---
+++ app/src/main/java/edu/gcu/today/TodayMain.java
@@ -0,0 +1,14 @@
+package edu.gcu.today;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TodayMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_today_main);
+    }
+}
