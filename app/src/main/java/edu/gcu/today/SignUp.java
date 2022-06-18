1fdc4dc94b7f8138d8f36e2be4c0f0335043d1ca
elale1984 <elale@mygcu.edu>
2022-06-18 01:50:27 -0500

    Fixed activity_sign_up.xml and added firebase gradle build script

--- app/src/main/java/edu/gcu/today/SignUp.java
+++ app/src/main/java/edu/gcu/today/SignUp.java
@@ -1,14 +1,32 @@
 package edu.gcu.today;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+
+import edu.gcu.today.databinding.ActivitySignUpBinding;
+
 
 public class SignUp extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    // Firebase
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
     }
 }
