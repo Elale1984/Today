05100a7f327f3959ff21d63e29f7a8ba0257ac89
elale1984 <elale@mygcu.edu>
2022-06-27 22:57:48 -0500

    fixed activity_sign_up.xml and created EventDateTime.java and EventHelperClass.java classes for data.

---
+++ app/src/main/java/edu/gcu/today/data/EventDateTime.java
@@ -0,0 +1,13 @@
+package edu.gcu.today.data;
+
+
+public class EventDateTime {
+
+    String day;
+    String month;
+    String year;
+    String hour;
+    String minute;
+    boolean aMPM;
+
+}
---
+++ app/src/main/java/edu/gcu/today/data/EventHelperClass.java
@@ -0,0 +1,14 @@
+package edu.gcu.today.data;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class EventHelperClass {
+
+   EventDateTime dateTime;
+   String eventName;
+   String details;
+
+
+
+}
